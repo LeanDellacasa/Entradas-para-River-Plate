019646fa7d72df2f25ab0f66ddae5c04c48a4226
LeanDellacasa <114032317+LeanDellacasa@users.noreply.github.com>
2022-12-14 11:11:30 -0300

    Create tickets.js

---
+++ tickets.js
@@ -0,0 +1,37 @@
+const tickets =  [
+    {
+        id: 1,
+        torneo: "Copa Libertadores",
+        rival: "Colo Colo",
+        precio: 5000,
+        estadio: "Local",
+        cantidad:0,
+    },
+
+    {
+        id: 2,
+        torneo: "Copa Libertadores",
+        rival: "Fluminense",
+        precio: 6000,
+        estadio: "Local",
+        cantidad:0,
+    },
+
+    {
+        id: 3,
+        torneo: "Torneo Argentino",
+        rival: "Boca",
+        precio: 8000,
+        estadio: "Visitante",
+        cantidad:0,
+    },
+
+    {
+        id: 4,
+        torneo: "Torneo Argentino",
+        rival: "Racing",
+        precio: 5500,
+        estadio: "Local",
+        cantidad:0,
+    }
+];
